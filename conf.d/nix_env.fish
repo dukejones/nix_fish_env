651a20e34cdd076072a9bd2e3db0a2077c067f67
Duke Jones <dukeofjones@gmail.com>
2019-06-19 23:48:26 +0200

    Install and support fish-nix-shell

--- conf.d/nix_env.fish
+++ conf.d/nix_env.fish
@@ -1,7 +1,30 @@
 
-if command -s pipenv > /dev/null
+if test -z (nix-env -q | grep fish-nix-shell)
+    while true
+        read -l -P 'Install fish-nix-shell? [Y/n] ' confirm
+
+        switch $confirm
+            case '' Y y
+                set_color green; echo "Installing fish-nix-shell"; set_color normal
+                nix-env -if https://github.com/haslersn/fish-nix-shell/archive/master.tar.gz
+                break
+            case N n
+                break
+        end
+    end
+end
+
+if command -s fish-nix-shell > /dev/null
+    fish-nix-shell --info-right | source
+end
+
+if command -s direnv > /dev/null
    eval (direnv hook fish)
 end
 
-set -gx NIX_BUILD_SHELL (which fish)
 bass source ~/.nix-profile/etc/profile.d/nix.sh
+
+# Consider any-nix-shell as alternative. 
+# https://github.com/haslersn/any-nix-shell
+# nix-env -i any-nix-shell -f https://github.com/NixOS/nixpkgs/archive/master.tar.gz
+# any-nix-shell fish --info-right | source
